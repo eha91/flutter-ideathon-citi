309901919c6bd8b66ac104e63f7ce88c118092c6
Paco <fgarcia@novasolutionsystems.com>
2020-11-08 10:05:57 -0600

    feat: add payment

--- lib/pages/payment.dart
+++ lib/pages/payment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class Payment extends StatefulWidget {
   Payment({Key key}) : super(key: key);
@@ -8,6 +9,9 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
+
+  int _state = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,84 +70,194 @@ class _PaymentState extends State<Payment> {
                   ],
                 )),
             Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Métodos de pago',
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
+              child: _state == 1 ? _paymetod() : _qr(),
+            ),
+
+            SizedBox(
+              height: 80,
+            ),
+            FlatButton(
+                onPressed: null,
+
+                child: Container(
+                  width: 250,
+                  height: 36,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.blueGrey,
+                      width: 1,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Card(
-                          child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            onTap: () {
-                              print('Card tapped.');
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 150,
-                              child: Center(child: Text('Código QR'),)
-                            ),
-                          ),
-                        ),
-                        Card(
-                          child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            onTap: () {
-                              print('Card tapped.');
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 150,
-                              child: Center(child: Text('Transferencia'),)
-                            ),
-                          ),
-                        ),
-                      
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Card(
-                          child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            onTap: () {
-                              print('Card tapped.');
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 150,
-                              child: Center(child: Text('Tarjeta de débito'),),
-                            ),
-                          ),
-                        ),
-                        Card(
-                          child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            onTap: () {
-                              print('Card tapped.');
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 150,
-                              child: Center( child: Text('Mercado pago'),)
-                            ),
-                          ),
-                        ),
-                      
-                      ],
-                    )
-                  
-                  ],
+                  ),
+                  child: Center(child: Text("Cancelar",style: TextStyle(fontSize: 16, color: Colors.blue),)),
                 ))
+          ],
+        ));
+  }
+
+  Widget _qr() {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Por favor escanee el código QR para realizar su pago.',
+            textAlign: TextAlign.left,
+          ),
+          Text(
+            'No se le cobrará ninguna comisión.',
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: 55,
+          ),
+          QrImage(
+            data: 'Los Forkis',
+            gapless: true,
+            foregroundColor: Colors.black,
+            size: 200,
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _paymetod () {
+    return Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Métodos de pago',
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Card(
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      setState(() {
+                        _state = _state + 1;
+
+                      });
+                    },
+                    child: Container(
+                        width: 150,
+                        height: 150,
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceAround,
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  'https://www.qrcode-monkey.com/img/blog/qrcode-classic-blue.png',
+                                  height: 80,
+                                ),
+                              ),
+                              Text("Código QR")
+                            ],
+                          ),
+                        )),
+                  ),
+                ),
+                Card(
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      print('Card tapped.');
+                    },
+                    child: Container(
+                        width: 150,
+                        height: 150,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceAround,
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  'https://cdn0.iconfinder.com/data/icons/banking-essentials/100/Finance_Cash_Transfer-512.png',
+                                  height: 80,
+                                ),
+                              ),
+                              Text("Transferencia")
+                            ],
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Card(
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      print('Card tapped.');
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceAround,
+                          children: [
+                            Center(
+                              child: Image.network(
+                                'https://i.pinimg.com/originals/71/81/e8/7181e84d50cb87fa4ab9a5a8ab613dbe.jpg',
+                                height: 80,
+                              ),
+                            ),
+                            Text("Mercado Pago")
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      print('Card tapped.');
+                    },
+                    child: Container(
+                        width: 150,
+                        height: 150,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceAround,
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  'https://media.istockphoto.com/vectors/mastercard-credit-blue-color-debit-card-icon-vector-id1225849081?k=6&m=1225849081&s=170667a&w=0&h=UWY7M4gXB3iEPnJmjwAQoPotOYFclJN2UOM8uBzQSu8=',
+                                  height: 80,
+                                ),
+                              ),
+                              Text(
+                                "Tarjeta de crédito/débito",
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            )
           ],
         ));
   }
