c05213d4cb86234f8a681f94c68fd6b4f7e4da8c
Paulina Guerrero <paaulinagp@gmail.com>
2020-11-07 19:53:07 -0600

    feat: dashboard

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white54);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Inicio',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Mi cartesa',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Pagar',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.person_rounded, 
+            color: Color.fromRGBO(5, 109, 174, 1)
+          ),
+          tooltip: 'Usuario',
+          onPressed: () {},
+        ),
+        title: Text(
+          'Dashboard', 
+          style: TextStyle(
+            color: Colors.black
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top:30),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 30
+              ),
+              child: Text(
+                '¡Hola Raymundo!',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(
+                      Icons.calendar_today,
+                      color: Color.fromRGBO(5, 109, 174, 1),
+                    ),
+                    title: Text('Citas agendadas'),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(
+                      Icons.account_balance_wallet_outlined,
+                      color: Color.fromRGBO(5, 109, 174, 1),
+                    ),
+                    title: Text('Mi cartera'),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(
+                      Icons.star_outline_outlined,
+                      color: Color.fromRGBO(5, 109, 174, 1),
+                    ),
+                    title: Text('Mi ranking'),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(
+                      Icons.local_offer_outlined,
+                      color: Color.fromRGBO(5, 109, 174, 1),
+                    ),
+                    title: Text('Promociones'),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(
+                      Icons.school_outlined,
+                      color: Color.fromRGBO(5, 109, 174, 1),
+                    ),
+                    title: Text('Educación financiera'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            label: 'Cartera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Pagar',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+        backgroundColor: Color.fromRGBO(58, 186, 237, 1),
+      ),
+    );
+  }
+}
